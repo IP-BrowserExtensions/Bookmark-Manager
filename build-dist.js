5843668c0ace6ab8c5b73d2172da61ddd7d40d73
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-09-20 00:29:29 +0300

    Updated config files

--- build-dist.js
+++ build-dist.js
@@ -2,63 +2,63 @@ const fs = require("fs");
 const path = require("path");
 
 const distPath = "./dist";
-const assetsPath = "./assets/images";
-const manifestPath = "./assets/manifest.json";
+const assetsPath = "./assets";
+const manifestPath = "./manifest.json";
 
 deleteFolderRecursive(distPath);
 copyFolderRecursiveSync(assetsPath, distPath);
 copyFileSync(manifestPath, distPath);
 
 function deleteFolderRecursive(target) {
-	if (target != "" && target != "./") {
-		if (fs.existsSync(target)) {
-			fs.readdirSync(target).forEach(function(file, index) {
-				var curPath = target + "/" + file;
-				if (fs.lstatSync(curPath).isDirectory()) {
-					// recurse
-					deleteFolderRecursive(curPath);
-				} else {
-					// delete file
-					fs.unlinkSync(curPath);
-				}
-			});
-			fs.rmdirSync(target);
-		}
-	}
+    if (target != "" && target != "./") {
+        if (fs.existsSync(target)) {
+            fs.readdirSync(target).forEach(function(file, index) {
+                var curPath = target + "/" + file;
+                if (fs.lstatSync(curPath).isDirectory()) {
+                    // recurse
+                    deleteFolderRecursive(curPath);
+                } else {
+                    // delete file
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(target);
+        }
+    }
 }
 
 function copyFolderRecursiveSync(source, target) {
-	var files = [];
+    var files = [];
 
-	//check if folder needs to be created or integrated
-	var targetFolder = path.join(target, path.basename(source));
-	if (!fs.existsSync(targetFolder)) {
-		fs.mkdirSync(targetFolder, { recursive: true });
-	}
+    //check if folder needs to be created or integrated
+    var targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder, { recursive: true });
+    }
 
-	//copy
-	if (fs.lstatSync(source).isDirectory()) {
-		files = fs.readdirSync(source);
-		files.forEach(function(file) {
-			var curSource = path.join(source, file);
-			if (fs.lstatSync(curSource).isDirectory()) {
-				copyFolderRecursiveSync(curSource, targetFolder);
-			} else {
-				copyFileSync(curSource, targetFolder);
-			}
-		});
-	}
+    //copy
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function(file) {
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
 }
 
 function copyFileSync(source, target) {
-	var targetFile = target;
+    var targetFile = target;
 
-	//if target is a directory a new file with the same name will be created
-	if (fs.existsSync(target)) {
-		if (fs.lstatSync(target).isDirectory()) {
-			targetFile = path.join(target, path.basename(source));
-		}
-	}
+    //if target is a directory a new file with the same name will be created
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
 
-	fs.writeFileSync(targetFile, fs.readFileSync(source));
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
