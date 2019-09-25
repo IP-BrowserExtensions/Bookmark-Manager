e8b97990477b24aeafd2ca9f7dda335b518546a4
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-09-26 02:06:17 +0300

    Wrappers implementation - WIP

---
+++ background-script/src/wrapper/chrome-wrapper/bookmark/bookmark-interface-wrapper.chrome.ts
@@ -0,0 +1,4 @@
+export interface IBookmarkTreeNode extends chrome.bookmarks.BookmarkTreeNode {}
+
+// write script to extract interfaces from chrome/ firefox namespaces from typings
+// go to github, on projects and add tasks
